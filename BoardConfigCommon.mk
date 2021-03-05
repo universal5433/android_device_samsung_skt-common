f2b4479c2120c50d69c9ef0631e7eb93e64d58d0
Anan Jaser <ananjaser@gmail.com>
2021-03-05 18:18:21 +0400

    skt-common: move board flags to common mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,1 +1,24 @@
-LOCAL_PATH := device/samsung/skt-common
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+COMMON_PATH := device/samsung/skt-common
+
+# Properties
+TARGET_SYSTEM_PROP := $(COMMON_PATH)/system.prop
+
+# RIL
+BOARD_MODEM_TYPE := ss333
+BOARD_PROVIDES_LIBRIL := true
